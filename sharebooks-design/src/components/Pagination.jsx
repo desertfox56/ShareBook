04ee4349e944e0bdab3c8bbf26305694fa25424a
Farid <mudrypingvin50@gmail.com>
2024-05-04 22:54:53 +0300

    Fixing React Pagination

--- sharebooks-design/src/components/Pagination.jsx
+++ sharebooks-design/src/components/Pagination.jsx
@@ -2,26 +2,28 @@ import React,{useEffect, useState} from 'react';
 import { Pagination } from 'antd';
 import axios from 'axios';
 function AppPagination(){
-  const [totalPages, setTotalPages ] = useState([]);
+  const [totalBooks, setTotalBooks ] = useState(0);
+  const booksPerPage = 10; // Adjust based on your backend settings or user choice
   useEffect(() => {
-    const fetchTotalPages = async () => {
+    const fetchTotalBooks = async () => {
       try {
         const response = await axios.get('http://localhost:8000/api/marketplace/pagination/');
-        setTotalPages(response.data.count);
+        setTotalBooks(response.data.count);
       } catch (error) {
         console.error('Error counting pages:', error);
     }
 };
 
-fetchTotalPages();
+fetchTotalBooks();
 }, []);
-
+const totalPages = Math.ceil(totalBooks / booksPerPage); // Calculate the total pages
     return(
         <Pagination style={{marginBottom:'30px'}}
         total={totalPages}
         showSizeChanger
         showQuickJumper
         showTotal={(total) => `Всего ${total} страниц`}
+        pageSize={booksPerPage}
       />
     );
 }
